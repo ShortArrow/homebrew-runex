1ce8a08f8369519f6dc24415ac6f2ca6c8ed2cd6
ShortArrow <bamboogeneral@shortarrow.jp>
2026-04-30 05:58:24 +0900

    runex: update to 0.1.12

--- Formula/runex.rb
+++ Formula/runex.rb
@@ -1,28 +1,28 @@
 class Runex < Formula
   desc "Cross-shell abbreviation engine that expands short tokens into full commands"
   homepage "https://github.com/ShortArrow/runex"
-  version "0.1.11"
+  version "0.1.12"
   license any_of: ["MIT", "Apache-2.0"]
 
   on_macos do
     on_arm do
       url "https://github.com/ShortArrow/runex/releases/download/v#{version}/runex-aarch64-apple-darwin.tar.gz"
-      sha256 "4c31b867574fc3e6a1e6d340ba9378437afa7a488c2edf7c5d8e13d5426f6bd7"
+      sha256 "ccbf86e8c89fb58614120a65ee214b69ef4692bb0d097c046121cfa584f75743"
     end
     on_intel do
       url "https://github.com/ShortArrow/runex/releases/download/v#{version}/runex-x86_64-apple-darwin.tar.gz"
-      sha256 "b8796a9d893ba8de44bd7ec268d9415c65f93393c7b39cc03ec97ee616b52df8"
+      sha256 "55a4e0571608bf23da567022beb77f604d08b440de2237440c2d184e606918a5"
     end
   end
 
   on_linux do
     on_arm do
       url "https://github.com/ShortArrow/runex/releases/download/v#{version}/runex-aarch64-unknown-linux-gnu.tar.gz"
-      sha256 "14f94242f7d151158ffaefbf90541e78d828d620b3a0588a8bbac6c2d9361422"
+      sha256 "4a801b77069fbfdaa1357135838d5968984006f98c75e952bdf0c0bfc141943f"
     end
     on_intel do
       url "https://github.com/ShortArrow/runex/releases/download/v#{version}/runex-x86_64-unknown-linux-gnu.tar.gz"
-      sha256 "1d2767e51ae575739ef97a048a27334c3d7c8b46550cd5ee400c85444b91e5fe"
+      sha256 "135b3cf784b457cbd5e9e92c89674f1115905ddfd19cfb56cf8b9b60f1cc9352"
     end
   end
 
